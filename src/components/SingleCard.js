b47ba6b21b0d2e62f52bad8ec6ee84072b877917
Kaiyana <kaiyanavlaustin@gmail.com>
2022-09-01 17:47:39 +0200

    Revert "still working on double click bug"
    This reverts commit a0d7b7847d35e1cef6d09c27b2a81abe33dad039.

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,24 +1,21 @@
 import React from 'react'
 import '../style/SingleCard.css'
-import _ from "lodash";
 
 export default function SingleCard({ card, handleChoice, flipped, disabled }) {
 
     const handleClick = () => {
-        
+        console.log('disabled')
         if (!disabled) {
             handleChoice(card)
         }
     }
-//still working on double click issue
-    const handledoubleClick = _.debounce(handleClick, 300)
 
     return (
         <div className="card">
             <div className={card.matched ? "matched" : ""}>
                 <div className={flipped ? "flipped" : ""}>
                     <img className="front" src={card.src} alt="card front" />
-                    <img className="back" src="/images/cover.png" onClick={handledoubleClick} onDoubleClick={handledoubleClick} alt="cover" />
+                    <img className="back" src="/images/cover.png" onClick={handleClick} alt="cover" />
                 </div>
             </div>
         </div>
