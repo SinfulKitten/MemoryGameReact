6e7cf4911ba5a792f7fd0dad2e2d370ec635dd94
Kaiyana <kaiyanavlaustin@gmail.com>
2022-09-05 20:07:07 +0200

    brought back input

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
 import { useNavigate} from 'react-router-dom';
 import '../style/Home.css';
-import Col from 'react-bootstrap/Col';
+import UserNameInputForm from '../components/UserNameInput';
+
 
 //Home page where players enter their player names and can click the 
 //start button to start the game. Uses html form elements and a button
@@ -27,26 +27,7 @@ export default function Home() {
             <h1>Memory</h1>
         </div>
         
-        <form>
-        <Col>
-          <img className="P1_sprite" src='/images/player1.png' alt="player_1_sprite"/>
-          <h2>Player 1</h2>
-        </Col>
-        <Col>
-          <h1>VS</h1>
-        </Col>
-        <Col>
-        <img className="P2_sprite" src='/images/player2.png' alt="player_2_sprite"/>
-        <h2>Player 2</h2>
-        </Col>
-        </form>
-        <br/>
-
-        <h2>Are you ready to play?</h2>
-
-        <div className="startButton">
-        <Button onClick={navAndCollect} >Lets Play</Button>
-        </div>    
-    </div>
+        <UserNameInputForm/>
+        </div>
   )
 }
